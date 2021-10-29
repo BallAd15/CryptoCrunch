1deea3f2d2144b5a587f90d99693cca495671078
Aditya Baliga <61866962+BallAd15@users.noreply.github.com>
2021-10-29 12:01:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,92 @@ client.remove_command("help")
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
   await client.change_presence(status = discord.Status.online,activity=discord.Activity(type=discord.ActivityType.watching, name='Otaku Service'))
+  
+  
+def send_json_request(ws, request):
+    ws.send(json.dumps(request))
 
+def recieve_json_response(ws):
+    response = ws.recv()
+    if response:
+        return json.loads(response)
+
+def heartbeat(interval, ws):
+    print('Heartbeat begin')
+    while True:
+        time.sleep(interval)
+        heartbeatJSON = {
+            "op": 1,
+            "d": "null"
+        }
+        send_json_request(ws, heartbeatJSON)
+        print("Heartbeat sent")
+
+def add_reaction(emoji,message_id):
+  headers={
+    'authorization':'NDMyNzE0MzE3NDU3MjYwNTY1.YXt-kQ.ndqlLwRIYHN3x5QUPjiZj8wjHfI'
+    }
+  r=requests.put(f'https://discord.com/api/v9/channels/699702250531979325/messages/{message_id}/reactions/{emoji}/%40me',headers=headers)
+
+ws = websocket.WebSocket()
+ws.connect('wss://gateway.discord.gg/?v=6&encording=json')
+event = recieve_json_response(ws)
+
+heartbeat_interval = event['d']['heartbeat_interval'] / 1000
+threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
+
+token = "NDMyNzE0MzE3NDU3MjYwNTY1.YXt-kQ.ndqlLwRIYHN3x5QUPjiZj8wjHfI"
+payload = {
+    'op': 2,
+    "d": {
+        "token": token,
+        "properties": {
+            "$os": "windows",
+            "$browser": "chrome",
+            "$device": 'pc'
+        }
+    }
+}
+send_json_request(ws, payload)
+
+while True:
+    event = recieve_json_response(ws)
+
+    try:
+        channel_id=int(event['d']['channel_id'])
+        #guild_id=int(event['d']['guild_id'])
+        author_id=int(event['d']['author']['id'])
+
+        if channel_id==699702250531979325 and author_id==617037497574359050:
+
+            restricted_ids=['194114491552628737','258297161740058624','903314702728319056',]
+
+            #print(f"{event['d']['author']['username']}: {event['d']['content']}")
+
+            string=str({event['d']['embeds'][0]['description']})
+
+            name_end=string.find("left a")
+
+            name=string[4:(name_end-2)]
+            if name in restricted_ids:
+                break
+            
+            time.sleep(2)
+            index = string.find('React w')
+
+            emojic = string[index+11:index+12]
+            msgid = int(event['d']['id'])
+
+
+            add_reaction(emojic, msgid)
+            print("Reacted")
+            op_code = event['op']
+            if op_code == 11:
+                print('heartbeat received')
+    except:
+        pass 
+
+  
 @client.command()
 async def leave(ctx):
   to_leave = client.get_guild(807116277029273630) 
